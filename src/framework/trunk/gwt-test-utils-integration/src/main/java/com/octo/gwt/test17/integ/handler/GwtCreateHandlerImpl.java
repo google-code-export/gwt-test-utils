1c390f918390b3b93f0f1a94acbbd83baa423d43
bertrand.paquet@gmail.com <bertrand.paquet@gmail.com@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2009-09-30 15:01:27 +0000

    Import some files from soft

---
+++ src/framework/trunk/gwt-test-utils-integration/src/main/java/com/octo/gwt/test17/integ/handler/GwtCreateHandlerImpl.java
@@ -0,0 +1,59 @@
+package com.octo.gwt.test17.integ.handler;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.octo.gwt.test17.GwtCreateHandler;
+
+public abstract class GwtCreateHandlerImpl implements GwtCreateHandler {
+
+	private static final Logger logger = Logger.getLogger(GwtCreateHandlerImpl.class);
+
+	private Map<Method, IDeserializationCallback> callbacks;
+
+	private IGwtRpcExceptionHandler exceptionHandler;
+
+	public GwtCreateHandlerImpl(Map<Method, IDeserializationCallback> callbacks, IGwtRpcExceptionHandler exceptionHandler) {
+		this.callbacks = callbacks;
+		this.exceptionHandler = exceptionHandler;
+	}
+
+	public abstract Object findService(String serviceName);
+	
+	public Object create(Class<?> classLiteral) {
+		logger.debug("Try to create class " + classLiteral.getCanonicalName());
+		try {
+			if (RemoteService.class.isAssignableFrom(classLiteral)) {
+				String asyncName = classLiteral.getCanonicalName() + "Async";
+				Class<?> asyncClazz = Class.forName(asyncName);
+				if (asyncClazz == null) {
+					throw new Exception("Async class not found : " + asyncName);
+				}
+				String serviceName = classLiteral.getSimpleName();
+				if (serviceName.startsWith("I")) {
+					serviceName = serviceName.substring(1);
+				}
+				logger.debug("Searching service named : " + serviceName);
+				Object service = findService(serviceName);
+				if (service == null) {
+					logger.error("Service not found " + serviceName);
+					throw new RuntimeException("Service not found " + serviceName);
+				}
+				InvocationHandler handler = new GwtRpcInvocationHandler(asyncClazz, service, callbacks, exceptionHandler);
+				return Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] { asyncClazz }, handler);
+			}
+			logger.debug("Creating class " + classLiteral.getCanonicalName());
+			Object o = classLiteral.newInstance();
+			return o;
+		} catch (Exception e) {
+			logger.error("Unable to create class", e);
+			throw new RuntimeException("Unable to create class " + classLiteral.getCanonicalName(), e);
+		}
+	}
+
+}
---
+++ src/framework/trunk/gwt-test-utils-integration/src/main/java/com/octo/gwt/test17/integ/handler/GwtRpcInvocationHandler.java
@@ -0,0 +1,83 @@
+package com.octo.gwt.test17.integ.handler;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.StatusCodeException;
+import com.octo.gwt.test17.ReflectionUtils;
+
+public class GwtRpcInvocationHandler implements InvocationHandler {
+
+	private static final Logger logger = Logger.getLogger(GwtRpcInvocationHandler.class);
+
+	private Object target;
+
+	private HashMap<Method, Method> methodTable;
+
+	private Map<Method, IDeserializationCallback> callbacks;
+	
+	private IGwtRpcExceptionHandler exceptionHandler;
+
+	public GwtRpcInvocationHandler(Class<?> asyncClazz, Object target, Map<Method, IDeserializationCallback> callbacks, IGwtRpcExceptionHandler exceptionHandler) {
+		this.target = target;
+		this.callbacks = callbacks;
+		this.exceptionHandler = exceptionHandler;
+		this.methodTable = new HashMap<Method, Method>();
+		for (Method m : asyncClazz.getMethods()) {
+			for (Method m2 : target.getClass().getMethods()) {
+				if (m.getName().equals(m2.getName()) && m.getParameterTypes().length == m2.getParameterTypes().length + 1) {
+					methodTable.put(m, m2);
+					m2.setAccessible(true);
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		Object[] subArgs = new Object[args.length - 1];
+		for(int i = 0; i < args.length - 2; i ++) {
+			subArgs[i] = args[i];
+		}
+		AsyncCallback<Object> callback = (AsyncCallback<Object>) args[args.length - 1];
+		Method m = methodTable.get(method);
+		if (m == null) {
+			logger.error("Method not found " + method);
+			callback.onFailure(new StatusCodeException(500, "No method found"));
+		}
+		try {
+			logger.debug("Invoking " + m + " on " + target.getClass().getCanonicalName());
+			// Serialize objects
+			Object[] serializedArgs = new Object[subArgs.length];
+			for (int i = 0; i < subArgs.length; i++) {
+				serializedArgs[i] = ReflectionUtils.serializeUnserialize(subArgs[i]);
+			}
+			Object returnValue = m.invoke(target, serializedArgs);
+			Object o = ReflectionUtils.serializeUnserialize(returnValue);
+			IDeserializationCallback deserializationCallback = callbacks.get(method);
+			if (deserializationCallback != null) {
+				o = deserializationCallback.callaback(o);
+			}
+			logger.debug("Result of " + m.getName() + " : " + o);
+			callback.onSuccess(o);
+		} catch (InvocationTargetException e) {
+			logger.info("Exception when invoking service throw to handler " + e.getMessage());
+			exceptionHandler.handle(e.getCause(), callback);
+			return null;
+		} catch (IllegalAccessException e) {
+			logger.error("Invokation exception : " + e.toString(), e);
+			callback.onFailure(new StatusCodeException(500, e.toString()));
+		} catch (IllegalArgumentException e) {
+			logger.error("Invokation exception : " + e.toString(), e);
+			callback.onFailure(new StatusCodeException(500, e.toString()));
+		}
+		return null;
+	}
+
+}
