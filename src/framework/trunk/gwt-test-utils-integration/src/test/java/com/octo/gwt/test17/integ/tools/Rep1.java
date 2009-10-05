2390478860fcf493279196ffef09727ac2ca205f
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2009-10-05 09:29:29 +0000

    Rename "functionnal-test" to "functional-tests"

--- src/framework/trunk/gwt-test-utils-integration/src/test/java/com/octo/gwt/test17/integ/tools/Rep1.java
+++ src/framework/trunk/gwt-test-utils-integration/src/test/java/com/octo/gwt/test17/integ/tools/Rep1.java
@@ -3,7 +3,7 @@ package com.octo.gwt.test17.integ.tools;
 import com.octo.gwt.test17.integ.CsvDirectory;
 import com.octo.gwt.test17.integ.CsvMacros;
 
-@CsvDirectory("functionnal-test/1")
+@CsvDirectory("functional-tests/1")
 @CsvMacros("macros")
 public class Rep1 extends AbstractGwtIntegrationShell {
 
