3de4f7e005c74cc1e95d0b2ed6a23ad0e958d33b
bertrand.paquet@gmail.com <bertrand.paquet@gmail.com@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2009-10-29 11:02:59 +0000

    Refactor test start

--- src/framework/trunk/gwt-test-utils/src/main/java/com/octo/gwt/test17/test/AbstractGWTTest.java
+++ src/framework/trunk/gwt-test-utils/src/main/java/com/octo/gwt/test17/test/AbstractGWTTest.java
@@ -8,7 +8,11 @@ import com.octo.gwt.test17.WidgetUtils;
 public abstract class AbstractGWTTest extends WidgetUtils {
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUpAbstractGwtTest() throws Exception {
+		initPatchGwt();
+	}
+	
+	protected void initPatchGwt() throws Exception {
 		//initialisation du framework de mock GWT
 		PatchGWT.init();
 		// reinit GWT
