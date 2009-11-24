88c093162c3ccf6ab2e483b1111a1e97fd250846
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2009-11-24 09:33:55 +0000

    Add a new test with code in patched constructor

--- src/framework/trunk/gwt-test-utils/src/test/java/com/octo/gwt/test17/JSONArrayTest.java
+++ src/framework/trunk/gwt-test-utils/src/test/java/com/octo/gwt/test17/JSONArrayTest.java
@@ -9,9 +9,19 @@ import com.octo.gwt.test17.test.AbstractGWTTest;
 public class JSONArrayTest extends AbstractGWTTest {
 
 	@Test
-	public void checkEmptyConstructor() throws Exception {
+	public void checkPatchEmptyConstructor() throws Exception {
 
 		PatchGWT.patch(JSONArray.class, new Patch[] { new PatchConstructor(null, new Class<?>[] {}), });
+
+		JSONArray jsonArray = new JSONArray();
+
+		Assert.assertNotNull("JSONArray instance should not be null", jsonArray);
+	}
+
+	@Test
+	public void checkPatchConstructorWithCode() throws Exception {
+
+		PatchGWT.patch(JSONArray.class, new Patch[] { new PatchConstructor("jsonArray = null", new Class<?>[] {}), });
 
 		JSONArray jsonArray = new JSONArray();
 
