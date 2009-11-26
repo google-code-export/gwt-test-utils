9133f6951d157b0dda19947360213874ce00f2d1
pkernevez <pkernevez@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2009-11-26 16:13:38 +0000

    Test qui pose pb pour pb @BeforeClass

--- src/framework/trunk/gwt-test-utils/src/test/java/com/octo/gwt/test17/GWTTest.java
+++ src/framework/trunk/gwt-test-utils/src/test/java/com/octo/gwt/test17/GWTTest.java
@@ -1,12 +1,32 @@
 package com.octo.gwt.test17;
 
+import static junit.framework.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.Locale;
+
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.octo.gwt.test17.test.AbstractGWTTest;
 
 public class GWTTest extends AbstractGWTTest {
+
+	private static String sToday;
+
+	@BeforeClass
+	public static void initToday() throws Exception {
+		PatchGWT.setLocale(new Locale("fr"));
+		sToday = DateTimeFormat.getFormat("EEE dd MMM").format(new Date(1259103600000l));
+	}
+
+	@Test
+	public void checkThatGwtInitialiseOccursBeforeTheJUnitInitialisationOfTheClass() {
+		assertEquals("mer. 25 nov.", sToday);
+	}
 
 	@Test
 	public void checkGetHostPageBase() {
