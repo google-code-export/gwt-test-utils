7859709bcf3a05952648eb949bca2336b8ce2a1a
pkernevez <pkernevez@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2009-11-29 23:42:26 +0000

    Add test for Patch constructor

---
+++ src/framework/trunk/gwt-test-utils/src/test/java/com/octo/gwt/test17/PatchConstructorTest.java
@@ -0,0 +1,47 @@
+package com.octo.gwt.test17;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
+
+import org.junit.Test;
+
+import com.octo.gwt.test17.test.AbstractGWTTest;
+
+public class PatchConstructorTest extends AbstractGWTTest {
+
+	@Test
+	public void checkThatConstructorArePatched() throws Exception {
+		//Fisrt check original constructors
+		new SimpleClassForPatchMethod();
+		assertEquals("Qwerty", new SimpleClassForPatchMethod("Qwerty").mInternal);
+
+		PatchGWT.patch(SimpleClassForPatchMethod.class, new Patch[] {
+				new PatchConstructor("throw new RuntimeException(\"Check one.\");", new Class<?>[] {}),
+				new PatchConstructor("throw new RuntimeException(\"Check two.\");", new Class<?>[] { String.class }) });
+		try {
+			new SimpleClassForPatchMethod();
+			fail("An exception should occured !");
+		} catch (RuntimeException e) {
+			assertEquals("Check one.", e.getMessage());
+		}
+		try {
+			new SimpleClassForPatchMethod("Qwerty");
+			fail("An exception should occured !");
+		} catch (RuntimeException e) {
+			assertEquals("Check two.", e.getMessage());
+		}
+
+	}
+
+	public static class SimpleClassForPatchMethod {
+		private String mInternal;
+
+		private SimpleClassForPatchMethod() {
+		}
+
+		private SimpleClassForPatchMethod(String pParam) {
+			mInternal = pParam;
+		}
+	}
+
+}
