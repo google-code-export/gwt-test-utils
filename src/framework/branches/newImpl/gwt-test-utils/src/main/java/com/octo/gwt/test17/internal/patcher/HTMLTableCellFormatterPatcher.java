5468b2cba172c5665bb2764b2c0fd2a6a2df590d
bertrand.paquet@gmail.com <bertrand.paquet@gmail.com@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2010-01-26 16:20:38 +0000

    Refactor HTMLTableCellFormater

--- src/framework/branches/newImpl/gwt-test-utils/src/main/java/com/octo/gwt/test17/internal/patcher/HTMLTableCellFormatterPatcher.java
+++ src/framework/branches/newImpl/gwt-test-utils/src/main/java/com/octo/gwt/test17/internal/patcher/HTMLTableCellFormatterPatcher.java
@@ -1,24 +1,17 @@
 package com.octo.gwt.test17.internal.patcher;
 
-import javassist.CtMethod;
-
 import com.google.gwt.dom.client.TableCellElement;
 import com.google.gwt.dom.client.TableRowElement;
 import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.HTMLTable;
 import com.octo.gwt.test17.ElementUtils;
+import com.octo.gwt.test17.ng.AutomaticPatcher;
+import com.octo.gwt.test17.ng.PatchMethod;
 
-public class HTMLTableCellFormatterPatcher extends AbstractPatcher {
+public class HTMLTableCellFormatterPatcher extends AutomaticPatcher {
 
-	@Override
-	public String getNewBody(CtMethod m) {
-		if (match(m, "getCellElement")) {
-			return callMethod("getCellElement", "$1, $2, $3");
-		}
-
-		return null;
-	}
-
-	public static Element getCellElement(Element table, int row, int col) {
+	@PatchMethod
+	public static Element getCellElement(HTMLTable.CellFormatter cellFormatter, Element table, int row, int col) {
 		TableRowElement rowElement = (TableRowElement) table.getChildNodes().getItem(row);
 		return ElementUtils.castToUserElement((TableCellElement) rowElement.getChildNodes().getItem(col));
 	}
