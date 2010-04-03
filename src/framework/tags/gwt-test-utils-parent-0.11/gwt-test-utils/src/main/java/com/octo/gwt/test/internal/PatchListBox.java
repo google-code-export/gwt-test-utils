edfb9b3187c48006ca07e1c74ce90a7eaeaf117b
bertrand.paquet <bertrand.paquet@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2010-04-03 06:33:36 +0000

    [maven-release-plugin]  copy for tag gwt-test-utils-parent-0.11

---
+++ src/framework/tags/gwt-test-utils-parent-0.11/gwt-test-utils/src/main/java/com/octo/gwt/test/internal/PatchListBox.java
@@ -0,0 +1,35 @@
+package com.octo.gwt.test.internal;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.SelectElement;
+import com.google.gwt.user.client.ui.ListBox;
+import com.octo.gwt.test.internal.dom.UserElement;
+import com.octo.gwt.test.internal.overrides.OverrideSelectElement;
+
+public class PatchListBox {
+
+	public static SelectElement getSelectElement(Object o) {
+		if (o instanceof ListBox) {
+			ListBox list = (ListBox) o;
+			UserElement e = UserElement.overrideCast(list.getElement());
+			Element elem = e.getOther();
+			if (elem instanceof SelectElement) {
+				SelectElement select = (SelectElement) elem;
+				return select;
+			}
+		}
+		throw new UnsupportedOperationException("Unable to cast to SelectElement " + o.getClass().getCanonicalName());
+	}
+
+	public static void clearListBox(Object o) {
+		if (o instanceof ListBox) {
+			ListBox list = (ListBox) o;
+			UserElement e = UserElement.overrideCast(list.getElement());
+			Element elem = e.getOther();
+			if (elem instanceof SelectElement) {
+				OverrideSelectElement s = OverrideSelectElement.overrideCast(elem);
+				s.overrideClear();
+			}
+		}
+	}
+}
---
+++ src/framework/tags/gwt-test-utils-parent-0.11/gwt-test-utils/src/main/java/com/octo/gwt/test/internal/overrides/OverrideBodyElement.java
@@ -0,0 +1,11 @@
+package com.octo.gwt.test.internal.overrides;
+
+import com.google.gwt.dom.client.BodyElement;
+
+public class OverrideBodyElement extends BodyElement {
+
+	public OverrideBodyElement() {
+
+	}
+
+}
