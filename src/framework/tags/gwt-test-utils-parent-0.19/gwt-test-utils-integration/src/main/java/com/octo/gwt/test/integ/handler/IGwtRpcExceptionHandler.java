9e3876a9c48f9cac9e912dac5658b56ae6261803
bertrand.paquet@gmail.com <bertrand.paquet@gmail.com@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2010-09-07 09:30:44 +0000

    Remove wrong tag

--- src/framework/tags/gwt-test-utils-parent-0.19/gwt-test-utils-integration/src/main/java/com/octo/gwt/test/integ/handler/IGwtRpcExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.octo.gwt.test.integ.handler;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface IGwtRpcExceptionHandler {
-
-	void handle(Throwable t, AsyncCallback<?> callback);
-
-}
--- src/framework/tags/gwt-test-utils-parent-0.19/gwt-test-utils/src/main/java/com/octo/gwt/test/GwtLogHandler.java
+++
@@ -1,7 +0,0 @@
-package com.octo.gwt.test;
-
-public interface GwtLogHandler {
-
-	void log(String message, Throwable t);
-
-}
