1e906e6f051cf8c2b5a7a26e469c0d8ba49147eb
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2011-12-17 00:26:15 +0000

    [maven-release-plugin]  copy for tag gwt-test-utils-parent-0.35

---
+++ src/framework/tags/gwt-test-utils-parent-0.35/gwt-test-utils/src/main/java/com/octo/gwt/test/internal/utils/StyleUtils.java
@@ -0,0 +1,57 @@
+package com.octo.gwt.test.internal.utils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style;
+import com.octo.gwt.test.internal.patchers.dom.JavaScriptObjects;
+
+/**
+ * 
+ * Some {@link Style} utility methods. <strong>For internal use only.</strong>
+ * 
+ * @author Gael Lazzari
+ * 
+ */
+public class StyleUtils {
+
+  private static final Pattern STYLE_PATTERN = Pattern.compile("(.+):(.+)");
+
+  public static Element getOwnerElement(Style style) {
+    return JavaScriptObjects.getObject(style,
+        JsoProperties.STYLE_TARGET_ELEMENT);
+  }
+
+  public static LinkedHashMap<String, String> getStyleProperties(String style) {
+    LinkedHashMap<String, String> result = new LinkedHashMap<String, String>();
+
+    if (style == null || style.trim().length() == 0) {
+      return result;
+    }
+
+    String[] styles = style.split("\\s*;\\s*");
+    for (String property : styles) {
+      Matcher m = STYLE_PATTERN.matcher(property);
+      if (m.matches()) {
+        result.put(m.group(1).trim(), m.group(2).trim());
+      }
+    }
+
+    return result;
+  }
+
+  public static void setStyle(Style target, String value) {
+    for (Map.Entry<String, String> entry : getStyleProperties(value).entrySet()) {
+      target.setProperty(GwtStringUtils.camelize(entry.getKey()),
+          entry.getValue());
+    }
+  }
+
+  private StyleUtils() {
+
+  }
+
+}
