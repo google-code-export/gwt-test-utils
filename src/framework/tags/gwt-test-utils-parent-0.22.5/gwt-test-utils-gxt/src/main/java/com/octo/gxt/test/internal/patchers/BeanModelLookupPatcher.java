38a1805d38b7351fef7dd8716991e808d1f0d969
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2011-12-31 10:19:44 +0000

    [maven-release-plugin]  copy for tag gwt-test-utils-parent-0.22.5

---
+++ src/framework/tags/gwt-test-utils-parent-0.22.5/gwt-test-utils-gxt/src/main/java/com/octo/gxt/test/internal/patchers/BeanModelLookupPatcher.java
@@ -0,0 +1,75 @@
+package com.octo.gxt.test.internal.patchers;
+
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+import com.extjs.gxt.ui.client.data.BeanModel;
+import com.extjs.gxt.ui.client.data.BeanModelFactory;
+import com.extjs.gxt.ui.client.data.BeanModelLookup;
+import com.octo.gwt.test.exceptions.ReflectionException;
+import com.octo.gwt.test.patchers.PatchClass;
+import com.octo.gwt.test.patchers.PatchMethod;
+
+@PatchClass(BeanModelLookup.class)
+class BeanModelLookupPatcher {
+
+  private static class BeanModelFactoryImpl extends BeanModelFactory {
+
+    @Override
+    protected BeanModel newInstance() {
+      return new BeanModelImpl();
+    }
+
+  }
+
+  private static class BeanModelImpl extends BeanModel {
+
+    private static final long serialVersionUID = -4320294118731275689L;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <X> X get(String property) {
+      try {
+        return (X) PropertyUtils.getProperty(getBean(), property);
+      } catch (Exception e) {
+        throw new ReflectionException("Error while reading property '"
+            + property + "' for object of type '"
+            + getBean().getClass().getName() + "'", e);
+      }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Map<String, Object> getProperties() {
+      try {
+        return PropertyUtils.describe(getBean());
+      } catch (Exception e) {
+        throw new ReflectionException(
+            "Error while reading properties for object of type '"
+                + getBean().getClass().getName() + "'", e);
+      }
+    }
+
+    @Override
+    public Collection<String> getPropertyNames() {
+      Collection<String> names = new ArrayList<String>();
+      for (PropertyDescriptor pd : PropertyUtils.getPropertyDescriptors(getBean().getClass())) {
+        names.add(pd.getDisplayName());
+      }
+
+      return names;
+    }
+
+  }
+
+  @PatchMethod
+  static BeanModelFactory getFactory(BeanModelLookup beanModelLookup,
+      Class<?> bean) {
+    return new BeanModelFactoryImpl();
+  }
+
+}
---
+++ src/framework/tags/gwt-test-utils-parent-0.22.5/gwt-test-utils/src/main/java/com/octo/gwt/test/internal/utils/GwtHtmlContentHandler.java
@@ -0,0 +1,134 @@
+package com.octo.gwt.test.internal.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Node;
+import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.dom.client.Text;
+import com.octo.gwt.test.internal.patchers.dom.JavaScriptObjects;
+
+/**
+ * SAX handler for GWT DOM parsing. <strong>For internal use only.</strong>
+ * 
+ * @author Gael Lazzari
+ * 
+ */
+class GwtHtmlContentHandler implements ContentHandler {
+
+  private static final Pattern ROOT_DOCUMENT_TAG_PATTERN = Pattern.compile("^HTML|HEAD|BODY$");
+
+  private Node currentNode;
+
+  private final boolean innerHTML;
+
+  private final List<Node> nodes = new ArrayList<Node>();
+
+  public GwtHtmlContentHandler(boolean innerHTML) {
+    this.innerHTML = innerHTML;
+  }
+
+  public void characters(char[] ch, int start, int length) throws SAXException {
+
+    String string = String.valueOf(ch, start, length);
+
+    if (string.length() > 0) {
+      Text text = Document.get().createTextNode(string);
+
+      if (currentNode != null) {
+        currentNode.appendChild(text);
+      } else {
+        // root text node
+        nodes.add(text);
+      }
+    }
+  }
+
+  public void endDocument() throws SAXException {
+  }
+
+  public void endElement(String nameSpaceURI, String localName, String rawName)
+      throws SAXException {
+
+    // FIXME : big hack because NekoHTML 1.9.13 (included in gwt-dev.jar) has a
+    // bug with HTML fragment parsing, see :
+    // http://stackoverflow.com/questions/7294525/nekohtml-sax-fragment-parsing
+    if (innerHTML && ROOT_DOCUMENT_TAG_PATTERN.matcher(localName).matches()) {
+      // ignore HTML/HEAD/BODY element
+      return;
+    }
+
+    currentNode = currentNode.getParentNode();
+  }
+
+  public void endPrefixMapping(String prefix) throws SAXException {
+  }
+
+  public NodeList<Node> getParsedNodes() {
+    return JavaScriptObjects.newNodeList(nodes);
+  }
+
+  public void ignorableWhitespace(char[] ch, int start, int end)
+      throws SAXException {
+  }
+
+  public void processingInstruction(String target, String data)
+      throws SAXException {
+  }
+
+  public void setDocumentLocator(Locator locator) {
+  }
+
+  public void skippedEntity(String arg0) throws SAXException {
+  }
+
+  public void startDocument() throws SAXException {
+  }
+
+  public void startElement(String nameSpaceURI, String localName,
+      String rawName, Attributes attributes) throws SAXException {
+
+    // FIXME : big hack because NekoHTML 1.9.13 (included in gwt-dev.jar) has a
+    // bug with HTML fragment parsing, see :
+    // http://stackoverflow.com/questions/7294525/nekohtml-sax-fragment-parsing
+    if (innerHTML && ROOT_DOCUMENT_TAG_PATTERN.matcher(localName).matches()) {
+      // ignore HTML/HEAD/BODY element
+      return;
+    }
+
+    Element element = Document.get().createElement(localName);
+
+    if (currentNode != null) {
+      currentNode.appendChild(element);
+    } else {
+      // root node
+      nodes.add(element);
+    }
+    currentNode = element;
+
+    for (int index = 0; index < attributes.getLength(); index++) {
+      String attrName = attributes.getLocalName(index);
+      String attrValue = attributes.getValue(index);
+
+      if ("style".equalsIgnoreCase(attrName)) {
+        StyleUtils.setStyle(element.getStyle(), attrValue);
+      } else if ("class".equalsIgnoreCase(attrName)) {
+        element.setClassName(attrValue);
+      } else {
+        element.setAttribute(attrName, attrValue);
+
+      }
+    }
+  }
+
+  public void startPrefixMapping(String prefix, String URI) throws SAXException {
+  }
+}
