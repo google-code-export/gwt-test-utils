91216750088a37b6b1141f90881835003309d218
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2011-12-31 10:17:14 +0000

    remove out of date 0.22.5-version

--- src/framework/tags/gwt-test-utils-parent-0.22.5/gwt-test-utils-0.22-branch/gwt-test-utils-csv/src/test/java/com/octo/gwt/test/csv/Rep2.java
+++
@@ -1,8 +0,0 @@
-package com.octo.gwt.test.csv;
-
-
-@CsvDirectory("functional-tests/2")
-@CsvMacros(value = "functional-tests/macros", pattern = "macros_params.*")
-public class Rep2 extends MyGwtShell {
-
-}
--- src/framework/tags/gwt-test-utils-parent-0.22.5/gwt-test-utils-0.22-branch/gwt-test-utils/src/test/java/com/octo/gwt/test/deferred/IGenerateWith.java
+++
@@ -1,5 +0,0 @@
-package com.octo.gwt.test.deferred;
-
-public interface IGenerateWith {
-
-}
