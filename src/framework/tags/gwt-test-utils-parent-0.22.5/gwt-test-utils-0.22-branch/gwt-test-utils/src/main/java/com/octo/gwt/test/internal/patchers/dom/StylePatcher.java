e7fba91b2638e797099f0833c7cf1f94eb23c48e
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2011-12-31 15:58:31 +0000

    [maven-release-plugin]  copy for tag gwt-test-utils-parent-0.22.5

---
+++ src/framework/tags/gwt-test-utils-parent-0.22.5/gwt-test-utils-0.22-branch/gwt-test-utils/src/main/java/com/octo/gwt/test/internal/patchers/dom/StylePatcher.java
@@ -0,0 +1,95 @@
+package com.octo.gwt.test.internal.patchers.dom;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Float;
+import com.google.gwt.dom.client.Style.Unit;
+import com.octo.gwt.test.internal.utils.GwtStringUtils;
+import com.octo.gwt.test.internal.utils.JsoProperties;
+import com.octo.gwt.test.internal.utils.StyleUtils;
+import com.octo.gwt.test.patchers.PatchClass;
+import com.octo.gwt.test.patchers.PatchMethod;
+
+@PatchClass(Style.class)
+class StylePatcher {
+
+  @PatchMethod
+  public static void clearBorderWidth(Style style) {
+    JavaScriptObjects.remove(style, JsoProperties.STYLE_BORDER_BOTTOM_WIDTH);
+    JavaScriptObjects.remove(style, JsoProperties.STYLE_BORDER_LEFT_WIDTH);
+    JavaScriptObjects.remove(style, JsoProperties.STYLE_BORDER_RIGHT_WIDTH);
+    JavaScriptObjects.remove(style, JsoProperties.STYLE_BORDER_TOP_WIDTH);
+  }
+
+  @PatchMethod
+  static void clearFloat(Style style) {
+    setPropertyImpl(style, "float", "");
+  }
+
+  @PatchMethod
+  static String getBorderWidth(Style style) {
+    return getPropertyImpl(style, JsoProperties.STYLE_BORDER_TOP_WIDTH);
+  }
+
+  @PatchMethod
+  static String getPropertyImpl(Style style, String propertyName) {
+    return JavaScriptObjects.getString(style, propertyName);
+  }
+
+  @PatchMethod
+  static void setBorderWidth(Style style, double value, Unit unit) {
+    double modulo = value % 1;
+    String completeValue = (modulo == 0) ? Integer.toString((int) value)
+        + unit.getType() : Double.toString(value) + unit.getType();
+    JavaScriptObjects.setProperty(style,
+        JsoProperties.STYLE_BORDER_BOTTOM_WIDTH, completeValue);
+    JavaScriptObjects.setProperty(style, JsoProperties.STYLE_BORDER_LEFT_WIDTH,
+        completeValue);
+    JavaScriptObjects.setProperty(style,
+        JsoProperties.STYLE_BORDER_RIGHT_WIDTH, completeValue);
+    JavaScriptObjects.setProperty(style, JsoProperties.STYLE_BORDER_TOP_WIDTH,
+        completeValue);
+  }
+
+  @PatchMethod
+  static void setFloat(Style style, Float value) {
+    setPropertyImpl(style, "float", value.getCssName());
+  }
+
+  @PatchMethod
+  static void setPropertyImpl(Style style, String propertyName,
+      String propertyValue) {
+    // treat case when propertyValue = "250.0px" => "250px" instead
+    propertyValue = GwtStringUtils.treatDoubleValue(propertyValue);
+
+    JavaScriptObjects.setProperty(style, propertyName, propertyValue);
+
+    Element owner = StyleUtils.getOwnerElement(style);
+    String styleAttribute = owner.getAttribute("style");
+
+    LinkedHashMap<String, String> styleProperties = StyleUtils.getStyleProperties(styleAttribute);
+
+    String cssProperyName = GwtStringUtils.hyphenize(propertyName);
+
+    if (styleProperties.containsKey(cssProperyName)) {
+      styleProperties.remove(cssProperyName);
+    }
+
+    if (propertyValue != null && propertyValue.trim().length() > 0) {
+      styleProperties.put(cssProperyName, propertyValue.trim());
+    }
+
+    StringBuilder sb = new StringBuilder();
+
+    for (Map.Entry<String, String> entry : styleProperties.entrySet()) {
+      sb.append(entry.getKey()).append(": ").append(entry.getValue()).append(
+          "; ");
+    }
+
+    owner.setAttribute("style", sb.toString());
+  }
+
+}
---
+++ src/framework/tags/gwt-test-utils-parent-0.22.5/gwt-test-utils-0.22-branch/gwt-test-utils/src/test/java/com/octo/gwt/test/resources/MyClientBundle.java
@@ -0,0 +1,28 @@
+package com.octo.gwt.test.resources;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.DataResource;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.TextResource;
+import com.google.gwt.user.client.impl.WindowImplIE.Resources;
+
+public interface MyClientBundle extends Resources {
+
+  public static final MyClientBundle INSTANCE = GWT.create(MyClientBundle.class);
+
+  public ImageResource doubleShouldThrowException();
+
+  @Source("css/testCssResource.css")
+  public TestCssResource testCssResource();
+
+  @Source("textResourceXml.xml")
+  public DataResource testDataResource();
+
+  public ImageResource testImageResource();
+
+  public TextResource textResourceTxt();
+
+  @Source("textResourceXml.xml")
+  public TextResource textResourceXml();
+
+}
