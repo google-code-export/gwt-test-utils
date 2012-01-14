0117ee11635eb563c7d4791447b03c02c73d0fc4
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2012-01-14 15:05:11 +0000

    [maven-release-plugin]  copy for tag gwt-test-utils-parent-0.22.5

---
+++ src/framework/tags/gwt-test-utils-parent-0.22.5/gwt-test-utils/src/main/java/com/octo/gwt/test/internal/patchers/LayoutPanelPatcher.java
@@ -0,0 +1,21 @@
+package com.octo.gwt.test.internal.patchers;
+
+import com.google.gwt.layout.client.Layout.AnimationCallback;
+import com.google.gwt.user.client.ui.LayoutCommand;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.octo.gwt.test.patchers.PatchClass;
+import com.octo.gwt.test.patchers.PatchMethod;
+import com.octo.gwt.test.utils.GwtReflectionUtils;
+
+@PatchClass(LayoutPanel.class)
+class LayoutPanelPatcher {
+
+  @PatchMethod
+  static void animate(LayoutPanel panel, final int duration,
+      final AnimationCallback callback) {
+    LayoutCommand layoutCmd = GwtReflectionUtils.getPrivateFieldValue(panel,
+        "layoutCmd");
+    layoutCmd.schedule(0, callback);
+  }
+
+}
