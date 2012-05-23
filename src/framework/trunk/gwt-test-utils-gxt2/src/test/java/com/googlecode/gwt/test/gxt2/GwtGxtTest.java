3bbce31160d8b285bde77e6e0a5e0e6a96f5e0e4
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2012-05-23 05:18:31 +0000

    Rename 'com.googlecode.gxt.test' package to 'com.googlecode.gwt.test.gxt2'

---
+++ src/framework/trunk/gwt-test-utils-gxt2/src/test/java/com/googlecode/gwt/test/gxt2/GwtGxtTest.java
@@ -0,0 +1,17 @@
+package com.googlecode.gwt.test.gxt2;
+
+import com.googlecode.gwt.test.GwtTest;
+
+public abstract class GwtGxtTest extends GwtTest {
+
+  @Override
+  public String getModuleName() {
+    return "com.extjs.gxt.samples.desktop.DesktopApp";
+  }
+
+  @Override
+  protected String getHostPagePath(String moduleFullQualifiedName) {
+    return "test.html";
+  }
+
+}
