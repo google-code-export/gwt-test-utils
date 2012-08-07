6dfbb5ca2401d10a3f3830a6743d90a1046ac8f5
gael.lazzari <gael.lazzari@d9eb14d4-a931-11de-b950-3d5b5f4ea0aa>
2012-08-07 05:18:36 +0000

    Fix [http://code.google.com/p/gwt-test-utils/issues/detail?id=154 issue 154] :  GXT Templates didn't have variables replaced

--- src/framework/trunk/gwt-test-utils-gxt2/src/main/java/com/googlecode/gwt/test/gxt2/internal/patchers/TemplatePatcher.java
+++ src/framework/trunk/gwt-test-utils-gxt2/src/main/java/com/googlecode/gwt/test/gxt2/internal/patchers/TemplatePatcher.java
@@ -1,56 +1,74 @@
 package com.googlecode.gwt.test.gxt2.internal.patchers;
 
+import java.util.List;
+import java.util.Map;
+
 import com.extjs.gxt.ui.client.core.Template;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.user.client.Element;
+import com.googlecode.gwt.test.internal.utils.JavaScriptObjects;
 import com.googlecode.gwt.test.patchers.PatchClass;
 import com.googlecode.gwt.test.patchers.PatchMethod;
 
+@SuppressWarnings("unchecked")
 @PatchClass(Template.class)
 class TemplatePatcher {
 
-   @PatchMethod
-   static Element appendInternal(JavaScriptObject t, Element el, JavaScriptObject values) {
-      Element template = t.cast();
-      el.setInnerHTML(template.getInnerHTML());
+   private static final String JS_ARRAY_INNER_LIST = "GXT_JS_ARRAY_INNER_LIST";
 
+   @PatchMethod
+   static Element appendInternal(final JavaScriptObject t, final Element el,
+            final JavaScriptObject values) {
+
+      el.setInnerHTML(applyInternal(t, values));
       return el;
-
    }
 
    @PatchMethod
-   static String applyInternal(JavaScriptObject t, JavaScriptObject values) {
+   static String applyInternal(final JavaScriptObject t, final JavaScriptObject values) {
       Element template = t.cast();
-      return template.getInnerHTML();
+      String templHtml = template.getInnerHTML();
+
+      List<Object> array = (List<Object>) JavaScriptObjects.getObject(values, JS_ARRAY_INNER_LIST);
+
+      if (array != null) {
+         for (int i = 0; i < array.size(); i++) {
+            Object val = array.get(i);
+            templHtml = templHtml.replaceAll("\\{" + i + "\\}", val.toString());
+         }
+      } else {
+         for (Map.Entry<String, Object> entry : JavaScriptObjects.entrySet(values)) {
+            templHtml = templHtml.replaceAll("\\{" + entry.getKey() + "\\}",
+                     entry.getValue().toString());
+         }
+      }
+
+      return templHtml;
    }
 
    @PatchMethod
-   static void compile(Template tem) {
-      //
+   static void compile(final Template tem) {
    }
 
    @PatchMethod
-   static JavaScriptObject create(String html) {
+   static JavaScriptObject create(final String html) {
       Element template = Document.get().createDivElement().cast();
       template.setInnerHTML(html);
-
       return template;
    }
 
    @PatchMethod
-   static String getHtml(JavaScriptObject t) {
+   static String getHtml(final JavaScriptObject t) {
       Element template = t.cast();
       return template.getInnerHTML();
-
    }
 
    @PatchMethod
-   static Element insertInternal(String method, JavaScriptObject t, Element el,
-            JavaScriptObject values) {
-      Element template = t.cast();
-      el.setInnerHTML(template.getInnerHTML());
+   static Element insertInternal(final String method, final JavaScriptObject t, final Element el,
+            final JavaScriptObject values) {
 
+      el.setInnerHTML(applyInternal(t, values));
       return el;
    }
 
--- src/framework/trunk/gwt-test-utils-gxt2/src/test/java/com/googlecode/gwt/test/gxt2/FormPanelTest.java
+++ src/framework/trunk/gwt-test-utils-gxt2/src/test/java/com/googlecode/gwt/test/gxt2/FormPanelTest.java
@@ -1,13 +1,18 @@
 package com.googlecode.gwt.test.gxt2;
 
-import static org.junit.Assert.assertTrue;
+import static org.fest.assertions.Assertions.assertThat;
 
 import org.junit.Test;
 
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.layout.FormLayout;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.MenuBar.Resources;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class FormPanelTest extends GwtGxtTest {
 
@@ -21,6 +26,28 @@ public class FormPanelTest extends GwtGxtTest {
       panel.add(new Image(resources.menuBarSubMenuIcon()));
 
       // Assert
-      assertTrue(((Image) panel.getWidget(0)).getUrl().contains("menuBarSubMenuIcon"));
+      assertThat(((Image) panel.getWidget(0)).getUrl()).contains("menuBarSubMenuIcon");
+   }
+
+   @Test
+   public void formWithTextField() {
+      // Arrange
+      Window window = new Window();
+      LayoutContainer formContainer = new LayoutContainer();
+      FormLayout layout = new FormLayout();
+      formContainer.setLayout(layout);
+      TextField<String> field = new TextField<String>();
+      field.setName("test");
+      field.setFieldLabel("Test");
+      formContainer.add(field);
+      window.add(formContainer);
+      window.render(RootPanel.getBodyElement());
+
+      // Act
+      window.show();
+
+      // Assert
+      assertThat(formContainer.getElement().getInnerHTML()).isEqualTo(
+               "<div role=\"presentation\" class=\"x-form-item \" tabindex=\"-1\"><label for=\"x-auto-13-input\" class=\"x-form-item-label\">Test:</label><div role=\"presentation\" class=\"x-form-element x-form-el-x-auto-13\" id=\"x-form-el-x-auto-13\"><div role=\"presentation\" class=\"x-form-field-wrap x-component\" id=\"x-auto-13\" style=\"width: 200px; \"><input type=\"text\" class=\" x-form-field x-form-text\" id=\"x-auto-13-input\" name=\"test\" tabIndex=\"0\" style=\"width: 200px; \"></input></div></div><div class=\"x-form-clear-left\" role=\"presentation\"></div></div>");
    }
 }
